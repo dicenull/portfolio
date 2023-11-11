21da1e08e6412178c3814f1744d245390fb58f64
dicenull <dicenull.dev@gmail.com>
2023-11-11 21:23:39 +0900

    名前を表示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends HookConsumerWidget {
@@ -6,9 +8,31 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
+    return const Scaffold(
+      body: _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size.width * .3;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Assets.icon.image(width: size, height: size),
+          Text(
+            'dicenull',
+            style: GoogleFonts.notoSansJavanese(
+              textStyle: const TextStyle(fontSize: 64),
+            ),
+          ),
+        ],
       ),
     );
   }
