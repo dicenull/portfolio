2f4e11532990cd9df6c1f8cb4c76e3e6853fef81
dicenull <dicenull.dev@gmail.com>
2023-11-11 23:12:49 +0900

    qrコード表示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class HomePage extends HookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,19 +20,22 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size.width * .3;
+    final mediaSize = MediaQuery.of(context).size;
+    final iconSize = mediaSize.width * .3;
+    final qrSize = mediaSize.width * .1;
 
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Assets.icon.image(width: size, height: size),
+          Assets.icon.image(width: iconSize, height: iconSize),
           Text(
             'dicenull',
             style: GoogleFonts.notoSansJavanese(
               textStyle: const TextStyle(fontSize: 64),
             ),
           ),
+          QrImageView(data: Uri.base.toString(), size: qrSize),
         ],
       ),
     );
