ba4e7014737833131821fd690d268d24089ba23e
dicenull <dicenull.dev@gmail.com>
2024-04-10 22:22:39 +0900

    ✨ FirebaseAnalyticsを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:app/app.dart';
+import 'package:app/firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseAnalytics.instance.logEvent(name: 'LaunchApp');
 
   runApp(const ProviderScope(child: App()));
 }
